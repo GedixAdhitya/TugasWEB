8c43da4f7eee582ef78c866ed62299d4e17d8f98
GedixAdhitya <55056009+GedixAdhitya@users.noreply.github.com>
2023-11-27 19:26:47 +0800

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,10 @@
+<?php
+    $server = "localhost";
+    $user = "root";
+    $password = "";
+    $database = "2101020049_adhitya";
+
+    $con = mysqli_connect($server,$user,$password,$database);
+
+
+?>
